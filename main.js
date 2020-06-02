766dfff5ad20a3300b0572adc3af8210e5e8a90a
Balkiss-hue <58786205+Balkiss-hue@users.noreply.github.com>
2020-06-02 12:26:54 +0100

    Designs

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict"
+let name = "Balkiss";
+let courses = ["Javascript", "Html", "Css", "React", "node.js"];
+
+console.log('my name is ${name}');
+for (let i = 0; i < courses.length; i++) {
+    console.log(courses[i])
+    
+}
+
+function myFunction(num1, num2){
+    return (num1 * num2)
+}
+myFunction (1, 2)
+
+
